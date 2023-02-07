14312f57fea8aa840090e752ad0515ac17ade069
unknown <sumitrawat30aug@gmail.com>
2023-02-07 12:26:12 +0530

    mapDispatchToProps and mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 //import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Home from './containers/HomeContainer';
+import HomeContainer from './containers/HomeContainer';
 //import User from './components/Home';
 function App() {
   return (
     <div className="App">
-      <h1>App Component Sumit Rawat</h1>
-      <Home data={{name:"Sumit Singh Rawat", age:76}}/>
-    </div>
+      <HomeContainer/>
+          </div>
   );
 }
 
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,15 @@
 import Home from "../components/Home";
+import {connect} from 'react-redux';
+import { addToCart } from "../Service/Actions/action";
 
-export  default Home;
+const mapStateToProps= state=>({
+
+})
+
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler: data=>dispatch(addToCart(data))
+})
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
+//export  default Home;
